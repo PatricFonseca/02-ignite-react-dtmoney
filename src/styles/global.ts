95a0d3826bd834f4ce0ec4d90e5152f05a530e23
PatricFonseca <patric.ccomp@gmail.com>
2021-04-12 22:53:52 -0300

    Primeiro commit

---
+++ src/styles/global.ts
@@ -0,0 +1,57 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  :root{
+      --background: #f2f0f5;
+      --red: #e52e4d;
+      --green: #33CC95;
+      --blue: #5429cc;
+
+      --blue-light: #6933ff;
+
+      --text-title: #363f5f;
+      --text-body: #969cb3;
+
+      --shape: #ffff;
+  }
+
+  *{
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+  }
+
+  //font-size: 16px (Desktop)
+  //1 rem = 16px
+  html{
+      @media( max-width: 1080px){
+          font-size: 93.75%; //15px
+      }
+
+      @media(max-width: 720px){
+          font-size: 87.5%; //14px
+      }
+  }
+
+  body{
+      background: var(--background);
+      -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button{
+      font-family: 'Poppins', sans-serif;
+      font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong{
+      font-weight: 600;
+  }
+
+  button{
+      cursor: Pointer;
+  }
+
+  [disabled]{
+      cursor: not-allowed;
+  }
+`;
