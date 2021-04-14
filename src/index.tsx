4196c76ebfeb63e53deb54bbea52a38bf7aea5dd
PatricFonseca <patric.ccomp@gmail.com>
2021-04-14 20:23:00 -0300

    ajustes projeto

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,61 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import { createServer, Model } from "miragejs";
 import { App } from "./App";
+
+createServer({
+	models: {
+		transaction: Model,
+	},
+
+	seeds(server) {
+		server.db.loadData({
+			transactions: [
+				{
+					id: 1,
+					title: "Transaction 1",
+					amount: 400,
+					type: "Deposit",
+					category: "Food",
+					createdAt: new Date("2021-02-12 09:00:00"),
+				},
+				{
+					id: 2,
+					title: "Transaction 2",
+					amount: 400,
+					type: "Withdraw",
+					category: "Food",
+					createdAt: new Date("2021-02-13 09:00:00"),
+				},
+			],
+		});
+	},
+
+	routes() {
+		this.namespace = "api";
+
+		this.get("/transactions", () => {
+			return this.schema.all("transaction");
+			// return [
+			// 	{
+			// 		id: 1,
+			// 		title: "Transaction 1",
+			// 		amount: 400,
+			// 		type: "Deposit",
+			// 		category: "Food",
+			// 		createdAt: new Date(),
+			// 	},
+			// ];
+		});
+
+		this.post("/transactions", (schema, request) => {
+			const data = JSON.parse(request.requestBody);
+
+			return schema.create("transaction", data);
+		});
+	},
+});
 
 ReactDOM.render(
 	<React.StrictMode>
