bb9b34f290637744dafa448a43c37dce6ae11b67
PatricFonseca <patric.ccomp@gmail.com>
2021-04-14 22:15:21 -0300

    alterado o transactionsContext para useTransactions(hook)

--- src/hooks/useTransactions.tsx
+++ src/hooks/useTransactions.tsx
@@ -1,5 +1,11 @@
-import { createContext, ReactNode, useEffect, useState } from "react";
-import { api } from "./services/api";
+import {
+	createContext,
+	ReactNode,
+	useEffect,
+	useState,
+	useContext,
+} from "react";
+import { api } from "../services/api";
 
 interface Transaction {
 	id: number;
@@ -60,4 +66,10 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
 			{children}
 		</TransactionsContext.Provider>
 	);
+}
+
+export function useTransactions() {
+	const context = useContext(TransactionsContext);
+
+	return context;
 }
